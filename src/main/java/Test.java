e533d89f3a8100e9783dc5c9dec5a1b001c23e9b
tsiory <hei.tsiory.234@gmail.com>
2025-12-01 16:19:08 +0300

    ajout du fichier Test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Test {
+    public void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/testdb";
+        String user = "postgres";
+        String password = "aina";
+
+        try{
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to database");
+            conn.close();
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
